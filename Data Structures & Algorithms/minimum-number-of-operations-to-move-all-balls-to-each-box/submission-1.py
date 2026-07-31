c2b887db7cad5b8876eaaa8e2fac99251d29756c
Jeffrey Lin <133841831+JeffreyLin1@users.noreply.github.com>
2026-07-31 19:08:45 -0400

    Add: minimum-number-of-operations-to-move-all-balls-to-each-box - submission-1

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-move-all-balls-to-each-box/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        ans = [0]* len(boxes)
+
+        for i in range(len(ans)):
+            for j in range(len(boxes)):
+                if boxes[j] == "1":
+                    ans[i] += abs(i-j)
+        return ans
